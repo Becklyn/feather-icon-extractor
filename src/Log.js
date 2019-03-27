379c2ef1369de46a253dadd87d1a6bf1a7c1f75f
Jannik Zschiesche <jz@becklyn.com>
2019-03-27 15:46:45 +0100

    Main implementation

---
+++ src/Log.js
@@ -0,0 +1,91 @@
+const {yellow, green, black, bgGreen, blue, red} = require("kleur");
+
+/**
+ *
+ */
+class Log
+{
+    /**
+     * Renders a section
+     *
+     * @param {string} text
+     */
+    section (text)
+    {
+        console.log("");
+        console.log(yellow(text));
+        console.log("");
+    }
+
+
+    /**
+     *
+     */
+    done ()
+    {
+        console.log(`  ... ${green("done")}`);
+    }
+
+
+    /**
+     *
+     * @param {string} text
+     */
+    comment (text)
+    {
+        console.log(`  // ${text}`);
+        console.log("");
+    }
+
+
+    /**
+     *
+     */
+    allDone ()
+    {
+        console.log("");
+        console.log("");
+        console.log(bgGreen(black("                  ")));
+        console.log(bgGreen(black(" All done.        ")));
+        console.log(bgGreen(black("                  ")));
+    }
+
+
+    /**
+     * @param {string} name
+     */
+    iconStart (name)
+    {
+        console.log(`  Generating icon ${blue(name)}.svg`)
+    }
+
+
+    /**
+     * @param {string} message
+     */
+    iconStep (message)
+    {
+        console.log(`    -> ${message}`);
+    }
+
+
+    /**
+     *
+     */
+    iconDone ()
+    {
+        console.log(`    ${green("done")}`);
+        console.log("");
+    }
+
+
+    /**
+     * @param {string} message
+     */
+    iconError (message)
+    {
+        console.log(`    ${red("Error")}: ${message}`);
+    }
+}
+
+module.exports = Log;
